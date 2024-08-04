0f2e146ec973a771c84b30e8db96999461844f59
apirrone <awanegenebayo@gmail.com>
2024-08-04 17:50:46 +0200

    only forward

--- legged_gym/envs/bdx/bdx_amp_config.py
+++ legged_gym/envs/bdx/bdx_amp_config.py
@@ -161,7 +161,7 @@ class BDXAMPCfg(LeggedRobotCfg):
             feet_air_time = 0.0
             collision = 0.0
             feet_stumble = 0.0
-            action_rate = 0.0
+            action_rate = -10.0
             stand_still = 0.0
             dof_pos_limits = 0.0
 
