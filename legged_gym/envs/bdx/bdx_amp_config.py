941b16b82f7b893e9641da5a8ba8da003ef82294
apirrone <awanegenebayo@gmail.com>
2024-08-07 10:36:40 +0200

    trying amp only

--- legged_gym/envs/bdx/bdx_amp_config.py
+++ legged_gym/envs/bdx/bdx_amp_config.py
@@ -184,8 +184,8 @@ class BDXAMPCfg(LeggedRobotCfg):
 
         class scales(LeggedRobotCfg.rewards.scales):
             termination = 0.0
-            # tracking_lin_vel = 0.05 * (1.5 * 1.0 / (0.005 * 6))
-            # tracking_ang_vel = 0.05 * (0.5 * 1.0 / (0.005 * 6))
+            tracking_lin_vel = 1.5 * 1.0 / (0.005 * 6)
+            tracking_ang_vel = 0.5 * 1.0 / (0.005 * 6)
             tracking_lin_vel = 0
             tracking_ang_vel = 0
             lin_vel_z = 0.0
@@ -244,10 +244,10 @@ class BDXAMPCfgPPO(LeggedRobotCfgPPO):
         amp_reward_coef = 2.0  # 2.0
         amp_motion_files = MOTION_FILES
         amp_num_preload_transitions = 2000000
-        amp_task_reward_lerp = 0.3
+        amp_task_reward_lerp = 0.0  # 0.3
         amp_discr_hidden_dims = [1024, 512]
 
-        disc_grad_penalty = 1  # original 10
+        disc_grad_penalty = 10  # original 10
 
         # min_normalized_std = [0.05, 0.02, 0.05] * 4
 
