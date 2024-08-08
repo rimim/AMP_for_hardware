c7a6d6e448bd675aef25c63a8e0b23460db84a41
apirrone <awanegenebayo@gmail.com>
2024-08-08 14:34:20 +0200

    trying to match isaacgymenvs

--- legged_gym/envs/bdx/bdx_amp_config.py
+++ legged_gym/envs/bdx/bdx_amp_config.py
@@ -81,7 +81,7 @@ class BDXAMPCfg(LeggedRobotCfg):
         # PD Drive parameters:
         control_type = "P"
         override_effort = True
-        effort = 1.0  # Nm
+        effort = 0.52  # Nm
         # effort = 20  # Nm
 
         stiffness_all = 10.0  # 4 [N*m/rad]
