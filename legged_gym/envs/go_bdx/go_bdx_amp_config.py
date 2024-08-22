59ecf8530ff74f03fe6a475f7f38eff4b2ce9c99
rimim <nomail@thanks.com>
2024-08-22 12:11:07 -0400

    Updated lin_vel_x

--- legged_gym/envs/go_bdx/go_bdx_amp_config.py
+++ legged_gym/envs/go_bdx/go_bdx_amp_config.py
@@ -239,7 +239,7 @@ class GOBDXAMPCfg(LeggedRobotCfg):
         heading_command = False  # if true: compute ang vel command from heading error
 
         class ranges:
-            lin_vel_x = [0.3, 0.3]  # min max [m/s]
+            lin_vel_x = [0.1836, 0.1836]  # min max [m/s]
             lin_vel_y = [0, 0]  # min max [m/s]
             ang_vel_yaw = [0, 0]  # min max [rad/s]
             heading = [0, 0]
