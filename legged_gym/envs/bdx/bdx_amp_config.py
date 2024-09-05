a7f400a08adfbccec426f46e050ad237d89e1be2
apirrone <awanegenebayo@gmail.com>
2024-09-05 10:03:20 +0200

    more domain rand and more noise

--- legged_gym/envs/bdx/bdx_amp_config.py
+++ legged_gym/envs/bdx/bdx_amp_config.py
@@ -191,9 +191,9 @@ class BDXAMPCfg(LeggedRobotCfg):
 
     class domain_rand:
         randomize_friction = True
-        friction_range = [0.95, 1.05]
+        friction_range = [0.9, 1.11]
         randomize_base_mass = True
-        added_mass_range = [-0.05, 0.05]
+        added_mass_range = [-0.1, 0.1]
         push_robots = False
         push_interval_s = 15
         max_push_vel_xy = 0.5  # 0.3
@@ -201,20 +201,20 @@ class BDXAMPCfg(LeggedRobotCfg):
         stiffness_multiplier_range = [0.95, 1.05]
         damping_multiplier_range = [0.95, 1.05]
         randomize_torques = True
-        torque_multiplier_range = [0.9, 1.1]
+        torque_multiplier_range = [0.85, 1.15]
         randomize_com = True
-        com_range = [-0.02, 0.02]
+        com_range = [-0.04, 0.04]
 
     class noise:
         add_noise = True
         noise_level = 1.0  # scales other values
 
         class noise_scales:
-            dof_pos = 0.01
-            dof_vel = 0.01  # 1.5
-            lin_vel = 0.01
-            ang_vel = 0.01
-            gravity = 0.01
+            dof_pos = 0.05
+            dof_vel = 0.05  # 1.5
+            lin_vel = 0.05
+            ang_vel = 0.05
+            gravity = 0.05
             height_measurements = 0.1
 
     class rewards(LeggedRobotCfg.rewards):
