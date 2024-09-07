5e69a735ec27772a6f3d576f605b263fa48c686f
apirrone <awanegenebayo@gmail.com>
2024-09-07 18:19:00 +0200

    lerp 0.1

--- legged_gym/envs/go_bdx/go_bdx_amp_config.py
+++ legged_gym/envs/go_bdx/go_bdx_amp_config.py
@@ -287,7 +287,7 @@ class GOBDXAMPCfgPPO(LeggedRobotCfgPPO):
         amp_reward_coef = 2.0  # 2.0
         amp_motion_files = MOTION_FILES
         amp_num_preload_transitions = 2000000
-        amp_task_reward_lerp = 0.2  # 0.3
+        amp_task_reward_lerp = 0.1  # 0.3
         amp_discr_hidden_dims = [1024, 512]
 
         # disc_grad_penalty = 1  # original 10 # TUNE ?
