a7bea54923f39fd283514ff7c0123bb445504f98
apirrone <awanegenebayo@gmail.com>
2024-09-07 19:09:30 +0200

    update

--- legged_gym/envs/bdx/bdx_amp_config.py
+++ legged_gym/envs/bdx/bdx_amp_config.py
@@ -255,7 +255,7 @@ class BDXAMPCfg(LeggedRobotCfg):
         heading_command = False  # if true: compute ang vel command from heading error
 
         class ranges:
-            lin_vel_x = [0.0, 0.2]  # min max [m/s]
+            lin_vel_x = [0.0, 0.15]  # min max [m/s]
             lin_vel_y = [0, 0]  # min max [m/s]
             ang_vel_yaw = [-0.4, 0.4]  # min max [rad/s]
             heading = [0, 0]
@@ -301,7 +301,7 @@ class BDXAMPCfgPPO(LeggedRobotCfgPPO):
         amp_task_reward_lerp = 0.2  # 0.3
         amp_discr_hidden_dims = [1024, 512]
 
-        disc_grad_penalty = 0.02  # original 10 # TUNE ?
+        disc_grad_penalty = 0.05  # original 10 # TUNE ?
 
         # min_normalized_std = [0.05, 0.02, 0.05] * 4
 
