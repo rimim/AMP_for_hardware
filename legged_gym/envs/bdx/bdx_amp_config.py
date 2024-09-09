9705aa8068e5043f0816d5fcc57957f70c668cf4
apirrone <awanegenebayo@gmail.com>
2024-09-09 18:41:51 +0200

    action scale 0.25, turn for real. a little more lin vel x : 0.15

--- legged_gym/envs/bdx/bdx_amp_config.py
+++ legged_gym/envs/bdx/bdx_amp_config.py
@@ -257,10 +257,10 @@ class BDXAMPCfg(LeggedRobotCfg):
         heading_command = False  # if true: compute ang vel command from heading error
 
         class ranges:
-            lin_vel_x = [0.0, 0.1]  # min max [m/s]
-            lin_vel_y = [-0.3, 0.3]  # min max [m/s]
+            lin_vel_x = [0.0, 0.15]  # min max [m/s]
+            lin_vel_y = [0.0, 0.0]  # min max [m/s]
             # ang_vel_yaw = [-0.7, 0.7]  # min max [rad/s]
-            ang_vel_yaw = [0.0, 0.0]  # min max [rad/s]
+            ang_vel_yaw = [-0.3, 0.3]  # min max [rad/s]
             heading = [0, 0]
             # lin_vel_x = [0.1, 0.2]  # min max [m/s]
             # lin_vel_y = [0.0, 0.0]  # min max [m/s]
