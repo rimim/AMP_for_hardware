dd0251b7d313afc8a723c5879944bdbad41a5425
apirrone <awanegenebayo@gmail.com>
2024-09-11 10:42:52 +0200

    small turn commands

--- legged_gym/envs/bdx/bdx_amp_config.py
+++ legged_gym/envs/bdx/bdx_amp_config.py
@@ -259,8 +259,7 @@ class BDXAMPCfg(LeggedRobotCfg):
         class ranges:
             lin_vel_x = [0.0, 0.1]  # min max [m/s]
             lin_vel_y = [0.0, 0.0]  # min max [m/s]
-            # ang_vel_yaw = [-0.7, 0.7]  # min max [rad/s]
-            ang_vel_yaw = [0.0, 0.0]  # min max [rad/s]
+            ang_vel_yaw = [-0.1, 0.1]  # min max [rad/s]
             heading = [0, 0]
             # lin_vel_x = [0.1, 0.2]  # min max [m/s]
             # lin_vel_y = [0.0, 0.0]  # min max [m/s]
