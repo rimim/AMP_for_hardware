7b9af4cc1f4b2056be9caf88e06ef00aa11dab57
apirrone <awanegenebayo@gmail.com>
2024-09-15 14:29:13 +0200

    bdx amp

--- legged_gym/envs/bdx/bdx_amp_config.py
+++ legged_gym/envs/bdx/bdx_amp_config.py
@@ -185,7 +185,7 @@ class BDXAMPCfg(LeggedRobotCfg):
         self_collisions = 1  # 1 to disable, 0 to enable...bitwise filter
         # default_dof_drive_mode = 0  # see GymDofDriveModeFlags (0 is none, 1 is pos tgt, 2 is vel tgt, 3 effort)
         disable_gravity = False
-        fix_base_link = True  # fixe the base of the robot
+        fix_base_link = False  # fixe the base of the robot
 
     # class normalization(LeggedRobotCfg.normalization):
     #     clip_observations = 5.0
