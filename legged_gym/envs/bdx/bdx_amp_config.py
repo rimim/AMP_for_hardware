81454ea51d0549a50572ad391856debb394bc917
apirrone <awanegenebayo@gmail.com>
2024-09-15 11:52:49 +0200

    bdx amp

--- legged_gym/envs/bdx/bdx_amp_config.py
+++ legged_gym/envs/bdx/bdx_amp_config.py
@@ -243,7 +243,7 @@ class BDXAMPCfg(LeggedRobotCfg):
             feet_air_time = 0.0
             collision = 0.0
             feet_stumble = 0.0
-            action_rate = -0.1
+            action_rate = -1.0
             stand_still = 0.0
             dof_pos_limits = 0.0
             # motion_imitation = 5.0
