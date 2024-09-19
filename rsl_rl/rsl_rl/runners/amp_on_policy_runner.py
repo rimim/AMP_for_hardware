adee44b758be27be5e2a379cf8ced889cecd4f19
rimim <nomail@thanks.com>
2024-09-18 23:45:09 -0400

    Fixed tensor device

--- rsl_rl/rsl_rl/runners/amp_on_policy_runner.py
+++ rsl_rl/rsl_rl/runners/amp_on_policy_runner.py
@@ -364,7 +364,7 @@ class AMPOnPolicyRunner:
 
     def load(self, path, load_optimizer=True):
         #loaded_dict = torch.load(path, map_location=torch.device('cuda:0'))
-        loaded_dict = torch.load(path, map_location=torch.device('cuda:0'))
+        loaded_dict = torch.load(path, map_location=torch.device(self.device))
         self.alg.actor_critic.load_state_dict(loaded_dict["model_state_dict"])
         self.alg.discriminator.load_state_dict(loaded_dict["discriminator_state_dict"])
         self.alg.amp_normalizer = loaded_dict["amp_normalizer"]
